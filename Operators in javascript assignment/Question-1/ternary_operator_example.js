c2fabf28f073547e8fb68f39174e42febb46c187
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-11-30 18:43:05 +0530

    Add files via upload

---
+++ Operators in javascript assignment/Question-1/ternary_operator_example.js
@@ -0,0 +1,7 @@
+// program to check can drive or not
+
+const age = 20;
+
+const result = age >= 18 ? "You can drive." : "You  can't drive.";
+
+console.log(result);
---
+++ Operators in javascript assignment/Question-2/comma_operator_example.js
@@ -0,0 +1,6 @@
+let num1 = 15,
+  num2 = 23,
+  num3 = 435,
+  num4 = 34;
+
+console.log(num1, num2, num3, num4);
