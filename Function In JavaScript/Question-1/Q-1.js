257ad63fcbd17fc6acb78c6f97e1b2712fea991c
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-12-20 03:40:17 +0530

    Add files via upload

---
+++ Function In JavaScript/Question-1/Q-1.js
@@ -0,0 +1,8 @@
+// 1. Create an arrow function called square that takes a number as an argument and returns its square. Use
+// the arrow function to calculate the square of a given number and display the result.
+
+let square = (num) => num * num;
+
+let result = square(5);
+
+console.log(`Square of number is : ${result}`);
---
+++ Function In JavaScript/Question-2/Q-2.js
@@ -0,0 +1,13 @@
+// Create a JavaScript function called generateGreeting that takes a name as an argument and returns a
+// personalized greeting message. Use this function to greet three different people.
+
+function generateGreeting(name) {
+  return `Hello ${name}, how's it going?`;
+}
+let result1 = generateGreeting("Abhi");
+let result2 = generateGreeting("Sandeep");
+let result3 = generateGreeting("Shubh");
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
